dd8d858f15522704d2dd9abddb2dcce42c580651
Jarrion <jaredhunt2217@gmail.com>
2023-01-03 23:10:20 -0500

    Fixing consistency with single vs double quotes

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,4 +1,4 @@
-const gridContainer = document.getElementById("grid-container");
+const gridContainer = document.getElementById('grid-container');
 const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8'];
   
     for (let i = 0; i < items.length; i++) {
