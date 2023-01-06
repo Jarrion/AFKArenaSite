9adfb44ed42009f2e9bd67b29926554bca33cd9e
Jarrion <jaredhunt2217@gmail.com>
2023-01-06 02:07:26 -0500

    added cookie functionality

--- scripts/generateRarityString.js
+++ scripts/generateRarityString.js
@@ -1,5 +1,8 @@
 const heroNames = ['Belinda', 'Cecilia', 'Eluard', 'Estrilda', 'Fawkes', 'Gwyneth', 'Hendrik', 'Lucius', 'Morrow', 'Oscar', 'Peggy', 'Raine', 'Rigby', 'Rosaline', 'Rowan', 'Scarlet', 'Thane', 'Walker', 'Angelo', 'Hogan', 'Mirael', 'Morvus', 'Merek', 'Ulric', 'Alaro', 'Anoki', 'Antandra', 'Brutus', 'Drez', 'Granit', 'Khasos', 'Kren', 'Numisu', 'Safiya', 'Satrana', 'Skreg', 'Skriath', 'Thali', 'Thesku', 'Tidus', 'Vurk', 'Warek', 'Ankhira', 'Golus', 'Saveas', 'Arkadios', 'Bloodsnarl', 'Baden', 'Daimon', 'Desira', 'Fane', 'Ferael', 'Grezhul', 'Hodgkin', 'Isabella', 'Izold', 'Kelthur', 'Nara', 'Oden', 'Silas', 'Shemira', 'Theowyn', 'Thoran', 'Torne', 'Treznor', 'Niru', 'Silvina', 'Vedan', 'Sezis', 'Steixius', 'Astar', 'Eironn', 'Gorvo', 'Kaz', 'Lorsan', 'Lyca', 'Mishka', 'Nemora', 'Pippa', 'Raku', 'Respen', 'Saurus', 'Seirus', 'Solise', 'Tasi', 'Ulmus', 'Arden', 'Ira', 'Ogi', 'Oku', 'Dreaf', 'Eletha', 'Alna', 'Athalia', 'Audrae', 'Twins', 'Flora', 'Haelus', 'Morael', 'Orthros', 'Talene', 'ATalene', 'Titus', 'WuKong', 'Zaphrael', 'Ezizh', 'AEzizh', 'Framton', 'Khazard', 'Leofric', 'Lucretia', 'Mehira', 'Mezoth', 'Mortas', 'Zikis', 'Zolrath', 'Ainz', 'Albedo', 'Arthur', 'Ezio', 'Joker', 'Leonardo', 'Melusina', 'Merlin', 'Nakoruru', 'PoPersia', 'Queen', 'Ukyo'];
 
+    var cookieValue = getCookie("rarityStringCookie");
+    alert(`Rarity string: ${cookieValue}`);
+
     const container = document.querySelector('.container');
     heroNames.forEach(name => {
       const hero = document.createElement('div');
@@ -88,5 +91,9 @@ const heroNames = ['Belinda', 'Cecilia', 'Eluard', 'Estrilda', 'Fawkes', 'Gwynet
       const rarityString = Array.from(selects).map(select => select.value[0]).join('');
       const rarityStringDiv = document.querySelector('#rarity-string-div');
       rarityStringDiv.textContent = `Rarity string: ${rarityString}`;
+      setCookie("rarityStringCookie", rarityString);
       alert('Rarity string generated!');
+      var cookieValue = getCookie("rarityStringCookie");
+      alert(`Rarity string: ${cookieValue}`);
+      
     });
