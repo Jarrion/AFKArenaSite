8a253e116c465c1021ba7643f370a497754285bd
Jarrion <jaredhunt2217@gmail.com>
2023-01-09 14:13:03 -0500

    Added manual string entry

--- scripts/generateHeroes.js
+++ scripts/generateHeroes.js
@@ -2,7 +2,7 @@ const heroes = [];
 const heroNames = ['Belinda', 'Cecilia', 'Eluard', 'Estrilda', 'Fawkes', 'Gwyneth', 'Hendrik', 'Lucius', 'Morrow', 'Oscar', 'Peggy', 'Raine', 'Rigby', 'Rosaline', 'Rowan', 'Scarlet', 'Thane', 'Walker', 'Angelo', 'Hogan', 'Mirael', 'Morvus', 'Merek', 'Ulric', 'Alaro', 'Anoki', 'Antandra', 'Brutus', 'Drez', 'Granit', 'Khasos', 'Kren', 'Numisu', 'Safiya', 'Satrana', 'Skreg', 'Skriath', 'Thali', 'Thesku', 'Tidus', 'Vurk', 'Warek', 'Ankhira', 'Golus', 'Saveas', 'Arkadios', 'Bloodsnarl', 'Baden', 'Daimon', 'Desira', 'Fane', 'Ferael', 'Grezhul', 'Hodgkin', 'Isabella', 'Izold', 'Kelthur', 'Nara', 'Oden', 'Silas', 'Shemira', 'Theowyn', 'Thoran', 'Torne', 'Treznor', 'Niru', 'Silvina', 'Vedan', 'Sezis', 'Steixius', 'Astar', 'Eironn', 'Gorvo', 'Kaz', 'Lorsan', 'Lyca', 'Mishka', 'Nemora', 'Pippa', 'Raku', 'Respen', 'Saurus', 'Seirus', 'Solise', 'Tasi', 'Ulmus', 'Arden', 'Ira', 'Ogi', 'Oku', 'Dreaf', 'Eletha', 'Alna', 'Athalia', 'Audrae', 'Twins', 'Flora', 'Haelus', 'Morael', 'Orthros', 'Talene', 'ATalene', 'Titus', 'WuKong', 'Zaphrael', 'Ezizh', 'AEzizh', 'Framton', 'Khazard', 'Leofric', 'Lucretia', 'Mehira', 'Mezoth', 'Mortas', 'Zikis', 'Zolrath', 'Ainz', 'Albedo', 'Arthur', 'Ezio', 'Joker', 'Leonardo', 'Melusina', 'Merlin', 'Nakoruru', 'PoPersia', 'Queen', 'Ukyo'];
 const storedString = document.querySelector('#stored-string');
 
-var cookieValue = getCookie("rarityStringCookie");
+let cookieValue = getCookie("rarityStringCookie");
 storedString.textContent = `Currently stored string: ${cookieValue}`;
 
 function generateHeroes() {
@@ -97,7 +97,7 @@ function generateHeroes() {
 
   const container = document.querySelector('.grid-container');
 
-    heroes.forEach((hero, index) => {
+    heroes.forEach((hero) => {
     const heroDiv = document.createElement('div');
     heroDiv.classList.add('hero', hero.rarity);
     heroDiv.style.backgroundColor = hero.color;
@@ -109,5 +109,11 @@ function generateHeroes() {
 
   });
 
+}
+
+function manualStringEntry(){
+  const rarityString = document.getElementById('rarity-string').value;
+  setCookie("rarityStringCookie", rarityString);
+  alert(`Cookie set to ${rarityString}`);
 }
 
