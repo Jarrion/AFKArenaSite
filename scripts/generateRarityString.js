3cdbb801ce56f910e9edea02dc8ecead6ba0d087
Jarrion <jaredhunt2217@gmail.com>
2023-01-09 14:13:48 -0500

    added editing existing saved string from cookies

--- scripts/generateRarityString.js
+++ scripts/generateRarityString.js
@@ -1,8 +1,15 @@
 const heroNames = ['Belinda', 'Cecilia', 'Eluard', 'Estrilda', 'Fawkes', 'Gwyneth', 'Hendrik', 'Lucius', 'Morrow', 'Oscar', 'Peggy', 'Raine', 'Rigby', 'Rosaline', 'Rowan', 'Scarlet', 'Thane', 'Walker', 'Angelo', 'Hogan', 'Mirael', 'Morvus', 'Merek', 'Ulric', 'Alaro', 'Anoki', 'Antandra', 'Brutus', 'Drez', 'Granit', 'Khasos', 'Kren', 'Numisu', 'Safiya', 'Satrana', 'Skreg', 'Skriath', 'Thali', 'Thesku', 'Tidus', 'Vurk', 'Warek', 'Ankhira', 'Golus', 'Saveas', 'Arkadios', 'Bloodsnarl', 'Baden', 'Daimon', 'Desira', 'Fane', 'Ferael', 'Grezhul', 'Hodgkin', 'Isabella', 'Izold', 'Kelthur', 'Nara', 'Oden', 'Silas', 'Shemira', 'Theowyn', 'Thoran', 'Torne', 'Treznor', 'Niru', 'Silvina', 'Vedan', 'Sezis', 'Steixius', 'Astar', 'Eironn', 'Gorvo', 'Kaz', 'Lorsan', 'Lyca', 'Mishka', 'Nemora', 'Pippa', 'Raku', 'Respen', 'Saurus', 'Seirus', 'Solise', 'Tasi', 'Ulmus', 'Arden', 'Ira', 'Ogi', 'Oku', 'Dreaf', 'Eletha', 'Alna', 'Athalia', 'Audrae', 'Twins', 'Flora', 'Haelus', 'Morael', 'Orthros', 'Talene', 'ATalene', 'Titus', 'WuKong', 'Zaphrael', 'Ezizh', 'AEzizh', 'Framton', 'Khazard', 'Leofric', 'Lucretia', 'Mehira', 'Mezoth', 'Mortas', 'Zikis', 'Zolrath', 'Ainz', 'Albedo', 'Arthur', 'Ezio', 'Joker', 'Leonardo', 'Melusina', 'Merlin', 'Nakoruru', 'PoPersia', 'Queen', 'Ukyo'];
 const storedString = document.querySelector('#stored-string');
 
-    var cookieValue = getCookie("rarityStringCookie");
+//This is used to trigger a change event to properly adjust the rarity colors when a rarityString is loaded from cookies
+const changeEvent = new Event('change'); 
+
+    //Grab the rarityString from browser cookies
+    let cookieValue = getCookie("rarityStringCookie"); 
+
+    //Display the currently stored rarityString to the user
     storedString.textContent = `Currently stored string: ${cookieValue}`;
+
 
     const container = document.querySelector('.container');
     heroNames.forEach(name => {
@@ -82,8 +89,9 @@ const storedString = document.querySelector('#stored-string');
         select.appendChild(option);
       });
       hero.appendChild(select);
-
       container.appendChild(hero);
+
+      select.dispatchEvent(changeEvent)
     });
 
     const generateButton = document.querySelector('#generate-button');
@@ -99,3 +107,57 @@ const storedString = document.querySelector('#stored-string');
       
       
     });
+
+    function setRarityOptions() {
+        const selects = document.querySelectorAll('select');
+        const rarityString = cookieValue;
+
+        // Loop through each dropdown menu
+        for (let i = 0; i < selects.length; i++) {
+          // Get the current dropdown menu and the corresponding character in the rarity string
+          const select = selects[i];
+          const rarityChar = rarityString[i % rarityString.length];
+      
+          // Set the value of the dropdown menu based on the corresponding character in the rarity string
+          console.log(`rarityChar: ${rarityChar}`);
+          switch (rarityChar) {
+            case 'a':
+              select.selectedIndex = 0;
+              break;
+            case 'b':
+              select.selectedIndex = 1;
+              break;
+            case 'c':
+              select.selectedIndex = 2;
+              break;
+            case 'd':
+              select.selectedIndex = 3;
+              break;
+            case 'e':
+              select.selectedIndex = 4;
+              break;
+            case 'f':
+              select.selectedIndex = 5;
+              break;
+            case 'g':
+              select.selectedIndex = 6;
+              break;
+            case 'h':
+              select.selectedIndex = 7;
+              break;
+            case 'i':
+              select.selectedIndex = 8;
+              break;
+            case 'j':
+              select.selectedIndex = 9;
+              break;
+            case 'k':
+              select.selectedIndex = 10;
+              break; 
+            case 'l':
+              select.selectedIndex = 11;
+              break;
+          }
+          select.dispatchEvent(changeEvent)
+        }
+      }
