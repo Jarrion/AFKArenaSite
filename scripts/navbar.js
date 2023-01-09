8d351c33ab81ae07c61dfa1e201c51611034b4a6
Jarrion <jaredhunt2217@gmail.com>
2023-01-09 16:53:19 -0500

    Removed heroSelect for not being used

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,4 @@
 function insertNavbar() {
     var navbarElement = document.getElementById("navbar");
-    navbarElement.innerHTML = '<a href="index.html">Code Entry</a><a href="roster.html">Code Generation</a><a href="heroSelect.html">Details</a>';
+    navbarElement.innerHTML = '<a href="index.html">Code Entry</a><a href="roster.html">Code Generation</a>';
 }
