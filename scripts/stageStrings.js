02aa0a9290802c048eb2d38736f74c0b5f3cf136
Jarrion <jaredhunt2217@gmail.com>
2023-01-09 21:14:34 -0500

    Added rough iteration of stage functionality

---
+++ scripts/stageStrings.js
@@ -0,0 +1,5 @@
+const Chapter1Stage1 = 'aaaaaaaaaaaaaaaaaadccabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const Chapter1Stage2 = [];
+const Chapter1Stage3 = [];
+const Chapter1Stage4 = [];
+const Chapter1Stage5 = [];
