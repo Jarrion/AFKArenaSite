71e68fb6ee4c9edf7828cc41413bd30921e36df3
Jarrion <jaredhunt2217@gmail.com>
2023-01-09 21:13:59 -0500

    added stage page to navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,4 @@
 function insertNavbar() {
     var navbarElement = document.getElementById("navbar");
-    navbarElement.innerHTML = '<a href="roster.html">Code Generation</a><a href="index.html">Code Entry</a>';
+    navbarElement.innerHTML = '<a href="roster.html">Code Generation</a><a href="index.html">Code Entry</a><a href="stages.html">Stages</a>';
 }
